75189f7d035eb87f8f31a043891ef5dec22d5c53
Aatman Vaidya <aatmanvaidya@gmail.com>
2026-01-24 18:25:34 +0100

    fix: training setup

--- experiments/instruction_finetuning/train_llama_guard.py
+++ experiments/instruction_finetuning/train_llama_guard.py
@@ -421,8 +421,7 @@ class Trainer:
             # tokenizer=self.tokenizer,
             processing_class=self.tokenizer,
             formatting_func=self.formatting_prompts_func,
-            max_seq_length=self.config.max_seq_length,
-            packing=False,
+            max_length=self.config.max_seq_length,
             callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
         )
 
